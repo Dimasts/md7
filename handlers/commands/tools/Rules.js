98344c084519e70592490aa959a71dd83be6245b
Dimasts <100791650+Dimasts@users.noreply.github.com>
2022-04-10 16:21:18 +0700

    Update Rules.js

--- handlers/commands/tools/Rules.js
+++ handlers/commands/tools/Rules.js
@@ -4,11 +4,6 @@ module.exports = {
     cmd: ['peraturan', 'rules'],
     help: ['rules'],
     exec: (m) => {
-        m.reply('*RULES PERATURAN REALM*
-- Wajib vaksin
-- Wajib pakai masker
-- Wajib cuci tangan
-- Wajib untuk mematuhi rules
-- rules diatas hanya sementara')
+        m.reply('*RULES PERATURAN REALM*\n- Wajib vaksin\n- Wajib pakai masker\n- Wajib cuci tangan\n- Wajib untuk mematuhi rules\n- rules diatas hanya sementara')
     }
 }
