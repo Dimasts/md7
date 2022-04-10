c956642e565608e080bc652e43c40ad7c52aa3f1
Dimasts <100791650+Dimasts@users.noreply.github.com>
2022-04-10 16:07:58 +0700

    Update Rules.js

--- handlers/commands/tools/Rules.js
+++ handlers/commands/tools/Rules.js
@@ -4,6 +4,11 @@ module.exports = {
     cmd: ['peraturan', 'rules'],
     help: ['rules'],
     exec: (m) => {
-        m.reply('RULES ON')
+        m.reply('RULES ON
+- 1
+- 2
+- 3
+- 4
+- 5')
     }
 }
