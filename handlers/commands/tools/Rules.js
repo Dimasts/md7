14da883bcc3d15c8ddf106aae6da3336a85e35c4
Dimasts <100791650+Dimasts@users.noreply.github.com>
2022-04-10 15:52:13 +0700

    Update Rules.js

--- handlers/commands/tools/Rules.js
+++ handlers/commands/tools/Rules.js
@@ -1,1 +1,12 @@
-rules.js
+
+module.exports = {
+    tags: ['rules'],
+    cmd: ['peraturan', 'min'],
+    help: ['rules'],
+    exec: (m) => {
+        m.reply('RULES ON
+                1.DILARANG BLA BLA BLA
+                2.DILARANG BLA BLA BLA
+                3.DILARANG BLA BLA BLA')
+    }
+}
