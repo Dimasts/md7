0a5358e3ada497da4601e78a7366112b506535fa
Dimasts <100791650+Dimasts@users.noreply.github.com>
2022-04-11 14:33:06 +0700

    Add files via upload

---
+++ handlers/commands/tools/rules.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tags: ['rules'],
+    cmd: ['peraturan', 'rules'],
+    help: ['rules'],
+    exec: (m) => {
+        m.reply('*_PERATURAN REALM_*\n- Berakal sehat\n- Dilarang ngecheat/grief\n- MELIHAT CHEATER APALAGI YANG BERBAHAYA LANGSUNG LAPOR ADMIN,\n- Dilarang ambil barang(KALO DAPET IZIN DARI PEMILIK YA GAPAPA)\n- Rusuh auto siksa\n- Dilarang nyerang teman sendiri\n- Usahakan intro bagi new mem\n- Bermainlah dengan jujur karna kejujuranmu sangat penting bagi dirimu sendiri\n- Melanggar Rules diatas hukuman ban tanpa toleransi\n- PENGEN LINK REALM MINTA KE wa.me/6285360065102')
+    }
+}
